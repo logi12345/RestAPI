abb4108edb3904186029168cc596369d738bb252
logi12345 <k1764003@kcl.ac.uk>
2019-06-25 00:01:04 +0100

    Refactor add readme.md

---
+++ README.md
@@ -0,0 +1,49 @@
+# Employee ReSTAPI
+
+Employee ReSTAPI that allows users to POST, DELETE, UPDATE, GET to a local store.
+
+## Getting Started
+
+1.Find a directory to store the project.
+2.Within the directory Clone this repo:
+```
+ git clone https://github.com/logi12345/RestAPI
+ ```
+## Prerequisites
+
+Make sure you have the following tools installed.
+
+```
+node.js
+postman
+nodemon
+```
+## How to start
+
+You can use node to start the applicartion
+
+```
+node app.js
+```
+You can use nodemon to continue working on your project while the server is running without connecting and disconnecting.
+
+### Installing nodemon
+
+To install globally to your path.
+
+```
+npm install -g nodemon
+```
+To install it localy for your project.
+
+```
+npm install --save-dev nodemon
+```
+
+## Starting with nodemon
+
+```
+nodemon  app.js
+```
+
+
--- app.js
+++ app.js
@@ -2,114 +2,106 @@ const express = require('express');
 const db = require('./db/db');
 const bodyParser = require('body-parser');
 
+
+const SUCCESS = "true";
+const FAIL = "false";
+
+//Initiialise express
 const app = express();
 
+//Setup body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.get('/api/v1/employees', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        messsage: 'employees recieved successfully',
-        employees: db
+//Setup response
+const responseText = (res, errorCode, isSuccessString, message, Result) =>{
+    res.status(errorCode).send({
+        success: isSuccessString,
+        messsage: message,
+        Result
     })
+}
+
+//Get all employees
+app.get('/api/v1/employees', (req, res) => {
+    responseText(res, 200, SUCCESS, 'Employees retrieved successfully',  db );
 });
 
-app.post('/api/v1/create', (req, res) => {
 
-    console.log(req.body);
+//Posting a new employee
+app.post('/api/v1/create', (req, res) => {
+    //Check the name field is not blank
     if (!req.body.name) {
-        return res.status(400).send({
-            success: 'false',
-            message: 'name is required'
-        });
+        return responseText(res, 400, FAIL, 'Name is required');
     }
+    //Check the role field is not blank
     if (!req.body.role) {
-        return res.status(400).send({
-            success: 'false',
-            message: 'role is required'
-        });
+        return esponseText(res, 400, FAIL, 'Role is required');
     }
-        let lastElementID = db[db.length-1]+1;
-        const employee = {
+    //Post a successfully created employee.
+    let lastElementID = db[(db.length) - 1].id;
+    const employee = {
         id: lastElementID + 1,
         name: req.body.name,
         role: req.body.role
     }
     db.push(employee);
-    return res.status(201).send({
-        success: 'true',
-        message: 'todo added successfully',
-        employee
-    })
-
+    return responseText(res, 200, SUCCESS, 'Employee Added successfully', employee);
 });
 
+//Retireve a single employee
 app.get('/api/v1/employee/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
     db.forEach((employee) => {
         if (employee.id === id) {
-            return res.status(200).send({
-                success: 'true',
-                message: 'employee retrieved successfully',
-                employee
-            })
+            return responseText(res, 200, SUCCESS, 'Employee retrieved successfully', employee);
         }
-
     })
-
-    return res.status(404).send({
-        success: 'false',
-        message: 'todo does not exist',
-    })
+    return responseText(res, 404, FAIL, 'Employee not found', employee);
 })
 
+//Delete an employee
 app.delete('/api/v1/employee/delete/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
-    for (let index = 0; index < db.length; index++){
-            if (db[index].id === id) {
-                db.splice(index, 1);
-                return res.status(200).send({
-                    success: 'true',
-                    message: 'Employee deleted successful',
-                })
-
-            }
+    for (let index = 0; index < db.length; index++) {
+        if (db[index].id === id) {
+            db.splice(index, 1);
+            return responseText(res, 200, SUCCESS, 'Employee deleted successfully', employee);
+        }
     }
-    return res.status(404).send({
-        success:'false',
-        message: 'Employee not found'
-
-    })
+    return responseText(res, 404, FAIL, 'Employee not found', employee);
 });
 
-app.put('/api/v1/employee/update/:id', (req,res)=>{
+
+//Update an employee record
+app.put('/api/v1/employee/update/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
     console.log(id);
-    let found;
-    let index;
-    for (let i = 0; i < db.length; i++) {
-        if(db[i].id === id){
+    let found;//Record to update
+    let index;//position in store
+
+    //loop until matching ids found
+    let i=0;
+    while (i < db.length){
+        if (db[i].id === id) {
             found = db[i];
             index = i;
-        } 
+            i=db.length;
+        }
+        ++i;
     }
-    console.log(found);
-    if(!found){
-        return res.status(404).send({
-            success:'false',
-            message: 'Employee not found'
-        })
+    
+    //Return a response for invalid id
+    if (!found) {
+        return responseText(res, 404, FAIL, 'Employee not found');
     }
-    if(!req.body.name && !req.body.role){
-        return res.status(404).send({
-            success:'false',
-            message: 'Nothing to update'
-        })
+    if (!req.body.name && !req.body.role) {
+        return responseText(res, 400, FAIL, 'Nothing to update');
     }
 
+    //update employee
     const employeeToUpdate = {
         id: found.id,
         name: req.body.name ? req.body.name : found.name,
@@ -117,15 +109,11 @@ app.put('/api/v1/employee/update/:id', (req,res)=>{
     }
 
     db.splice(index, 1, employeeToUpdate)
-    return res.status(201).send({
-        success: 'true',
-        message: 'employee updated successfully',
-        employeeToUpdate
-    })
-
+    return responseText(res, 200, SUCCESS, 'Employee updated successfully', employeeToUpdate);
 })
 
+//listener for connecting to server
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log("Conneceted to PORT " + PORT)
+    console.log("Conneceted to PORT " + PORT);
 });
