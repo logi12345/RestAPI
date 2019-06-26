a58247810b94c1aea3989489a3e6e92f75017960
Logithan Gunaratnam <Logithan.Gunaratnam@bjss.com>
2019-06-26 13:31:17 +0100

    Change mock to clone dummy API

--- app.js
+++ app.js
@@ -16,79 +16,77 @@ app.use(bodyParser.urlencoded({
 
 //Get all employees
 app.get('/api/v1/employees', (req, res) => {
-    return db;
+    res.send(db);
 });
 
+const responseHandler = (object,res) => {
+    res.send(object)
+}
 
 const handleError =(req) =>{
-    if(db.length==0){
-        return false
-    }
-    if (!req.body.employee_name) {
-        return true;
+    if (!req.body.name) {
+       return true;
     }
     //Check the salary field is not blank
-    if (!req.body.employee_salary) {
-        return true;
+    if (!req.body.salary) {
+       return true;
     }
 
-    if (!req.body.employee_age) {
-        return true;
+    if (!req.body.age) {
+       return true;
     }
-
-    if (!req.body.profile_image){
-        return true;
-    }
-    return false;
+    return false
 }
 
 //Posting a new employee
 app.post('/api/v1/create', (req, res) => {
     //handle errors
     if (handleError(req)){
-        return false;
+       return responseHandler(false,res);
     }
     //Post a successfully created employee.
     let lastElementID = db.length ===0 ? 0 : db[db.length - 1].id;
     const employee = {
         id: lastElementID + 1,
-        employee_name: req.body.employee_name,
-        employee_salary: req.body.employee_salary,
-        employee_age: req.body.employee_age
+        employee_name: req.body.name,
+        employee_salary: req.body.salary,
+        employee_age: req.body.age,
+        profile_image: req.body.profile_image ?  req.body.profile_image : ""
     }
     db.push(employee);
-    return  employee;
+    return  responseHandler(employee,res);
 });
 
 //Retireve a single employee
 app.get('/api/v1/employee/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
     if(db.length ==0){
-        return false;
+        return responseHandler(false,res);
     }
-    db.forEach((employee) => {
-        if (employee.id === id) {
-            return employee;
+
+    for (let index = 0; index < db.length; index++) {
+        if (db[index].id === req.params.id) {
+            return responseHandler(db[index],res);
         }
-    })
-    return false;
+    }
+    return responseHandler(false,res);
 })
 
 //Delete an employee
 app.delete('/api/v1/delete/:id', (req, res) => {
     if(db.length ==0){
-        return false;
+        return responseHandler(false,res);
     }
     const id = parseInt(req.params.id, 10);
     for (let index = 0; index < db.length; index++) {
-        if (db[index].id === id) {
+        if (db[index].id === req.params.id) {
             const deletedEmployee = db[index]
             db.splice(index, 1);
             
-           return deletedEmployee;
+           return responseHandler(deletedEmployee,res);
         }
     }
-    return false;
+    return responseHandler(false,res);
 });
 
 
@@ -97,7 +95,7 @@ app.put('/api/v1/update/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
     
     if(db.length ==0){
-        return false;
+        responseHandler(false,res);
     }
     let found;//Record to update
     let index;//position in store
@@ -105,7 +103,7 @@ app.put('/api/v1/update/:id', (req, res) => {
     //loop until matching ids found
     let i=0;
     while (i < db.length){
-        if (db[i].id === id) {
+        if (db[i].id === req.params.id) {
             found = db[i];
             index = i;
             i=db.length;
@@ -115,22 +113,23 @@ app.put('/api/v1/update/:id', (req, res) => {
     
     //Return a response for invalid id
     if (!found) {
-        return false;
+        return responseHandler(false, res);
     }
-    if (!req.body.employee_name && !req.body.employee_salary, !req.body.employee_age) {
-        return false;
+    if (!req.body.name && !req.body.salary && !req.body.age) {
+        return responseHandler(false, res);
     }
 
     //update employee
     const employeeToUpdate = {
         id: found.id,
-        employee_name: req.body.employee_name ? req.body.employee_name : found.employee_name,
-        employee_salary: req.body.employee_salary ? req.body.employee_salary : found.employee_salary,
-        employee_age: req.body.employee_age ? req.body.employee_age: found.employee_age
+        employee_name: req.body.name ? req.body.name : found.employee_name,
+        employee_salary: req.body.salary ? req.body.salary : found.employee_salary,
+        employee_age: req.body.age ? req.body.age: found.employee_age,
+        profile_image: req.body.profile_image ?  req.body.profile_image : found.profile_image
     }
 
     db.splice(index, 1, employeeToUpdate)
-    return responseText(res, 200, SUCCESS, 'Employee updated successfully', employeeToUpdate);
+    return responseHandler(employeeToUpdate, res);
 })
 
 //listener for connecting to server
